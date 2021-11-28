bd2115c80c07cf1077da683d7c38c1b58d84ce8d
indrasupendisinaga <indrasupendisinaga@gmail.com>
2021-11-28 11:46:55 +0700

    edit

--- Latihan_Hellowworld.py
+++ Latihan_Hellowworld.py
@@ -1,6 +1,16 @@
-print ("hellow world")
-a=10
-print ("variable a bernilai", a)
+x = True
+y = False
+z = False
+
+if not x or y:
+    print(1)
+elif not x or not y and z:
+    print(2)
+elif not x or y or not y and x:
+    print(3)
+else:
+    print(4)
+    
 
 #cara mengompile python ke yang namanya bytecode
 #caranya adalah
